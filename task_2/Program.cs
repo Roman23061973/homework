74d333ba7cbda80b34f5fa150f306fb672b8adf7
Roman23061973 <ya.androm2014@ya.ru>
2022-08-09 19:52:47 +0300

    home

---
+++ task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int a = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if(max < b) max = b;
+if(max < c) max = c;
+Console.Write("Максимум - ");
+Console.WriteLine(max);
