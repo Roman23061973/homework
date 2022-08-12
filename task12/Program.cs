6a41fdebb405ba7cf84ba51c32eaeffc8d13fd8e
roman <ya.androm2014@yandex.ru>
2022-08-13 01:17:11 +0300

    task12

---
+++ task12/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Введите номер дня недели (например:1-понедельник... 7-воскресенье):");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (namber<1 || namber>7)
+
+{
+    Console.WriteLine("Error. Введена неправильная цифра.");
+
+}
+
+else if (number>5)
+
+{
+    Console.WriteLine("Да, это выходной день.");
+
+}
+
+else (namber<6);
+
+{
+
+    Console.WriteLine("Нет, это не выходной.");
+    
+}  
+    
+    
