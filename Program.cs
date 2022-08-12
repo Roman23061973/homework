7ef5ce2e1b75210c5128f334c872a43b1861e114
roman <ya.androm2014@yandex.ru>
2022-08-13 00:29:45 +0300

    task9

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine9);
+if (number>=100 $ number<1000)
+
+{
+
+    int digit2 = (number % 100)/10;
+    Console.WriteLine($"Вторая цифра этого числа.");
+}
+
+else
+
+{
+
+    Console.WriteLine("Error. Введите трехзначное число.");
+}
