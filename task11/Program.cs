590311775d45e7ba029562e56cd33d21340cb152
roman <ya.androm2014@yandex.ru>
2022-08-13 00:57:04 +0300

    task11

---
+++ task11/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a%7==0 & a%23==0)
+
+{
+    Console.WriteLine($"Число {a} кратно 7 и 23");
+
+}
+
+else
+
+{
+     Console.WriteLine($"Число {a} некратное");
+
+}
